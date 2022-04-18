5fbf4a66a38880b47b3e4c1f798262a1b4c3a919
libiquits <libiquits123@gmail.com>
2022-04-18 17:45:44 -0300

    Landing Page React

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="main">
+       <div className="center">
+
+        <div className="menu">
+          <div className="logo">
+            <h3>Kakas's Code</h3>
+          </div>
+          <div className="item-menu">
+            <a href="#">Login</a>
+          </div>
+        </div>
+        <div className="form">
+          <form>
+            <h2>Contact us!</h2>
+            <div className="items-form">
+              <input placeholder='Your name' type="text"/>
+              <input placeholder='Your E-mail' type="text"/>
+              <input placeholder='Your Phone'type="text"/>
+              <input placeholder='Your CPF'type="text"/>
+              <input type="submit"/>
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+
+      <div className='conteudo'>
+        <div className='center'>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo</p>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
